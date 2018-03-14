b125027a00d4a9c15da77d61d80d03b93af381bd
Bhargava Vadlamani <bharvadl@Bhargavas-MacBook-Pro.local>
2018-03-14 06:28:40 -0400

    Code changed

--- kafka_services/api_service.py
+++ kafka_services/api_service.py
@@ -1,33 +1,22 @@
 import time
-from kafka import SimpleProducer, KafkaClient
 import requests
 import json
 import urllib2
+from kafka import SimpleProducer, KafkaClient
 from kafka import KafkaProducer
 from datetime import datetime
 from time import sleep
 
-
-
-# kafka = KafkaClient('localhost:8080')
 
 kafka = KafkaClient("localhost:9093")
 producer = SimpleProducer(kafka)
 
 topic = 'cex.io'
 
-# while 1:
-#   # "kafkaesque" is the name of our topic
-#   producer.send_messages("kafkaesque", ""+ str(datetime.now().time()) )
-#   sleep(1)
-
 def send_message(api_name=None):
     req=urllib2.Request("https://cex.io/api/currency_limits",headers={'User-Agent' : "Magic Browser"})
     data = json.loads(urllib2.urlopen(req).read())
-    # print data
     return str(data)
-    return str(datetime.now())
-
 
 #
 if __name__=="__main__":
