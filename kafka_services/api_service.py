8a6343f454efa27bdd6d7ea55ea4492bfec7e886
Bhargava Vadlamani <bharvadl@Bhargavas-MacBook-Pro.local>
2018-03-15 23:05:38 -0400

    Fixed bug-(Push onlly once)

--- kafka_services/api_service.py
+++ kafka_services/api_service.py
@@ -4,6 +4,7 @@ from kafka import SimpleProducer, KafkaClient
 from datetime import datetime
 from time import sleep
 import yaml
+from kafka.structs import TopicPartition
 
 kafka = KafkaClient("localhost:9093")
 producer = SimpleProducer(kafka)
@@ -15,19 +16,23 @@ def get_response(api):
         data = json.loads(urllib2.urlopen(req).read())
         return str(data)
     except Exception as e:
+        print "Get Response not working!"
         print e
 
 if __name__=="__main__":
 
     def lowda1():
         stream = open("ap_config.yaml","r")
-        yaml_obj = yaml.load_all(stream)
-        for config_obj in yaml_obj:
-            for key,val in config_obj.items():
-                return key,val
+        for line in  stream.readline():
+            yield line
 
     while True:
-        key,val = lowda1()
-        print "Publishing messages for ",key
-        producer.send_messages(key,get_response(val))
-        sleep(5)
+
+        with open("ap_config.yaml","r") as stream:
+            yaml_obj = yaml.load_all(stream)
+
+            for config_obj in yaml_obj:
+                for key,val in config_obj.items():
+                    producer.send_messages(key,get_response(val))
+                    print "Published messages to topic",key
+            sleep(5)
