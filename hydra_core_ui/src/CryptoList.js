64b7ad9a2f92e8ae5834c6e34ce9e2a70d6d984d
Prashanth Bheemagani <pbheemagani@expedia.com>
2018-03-16 13:31:25 -0400

    improve home page UI

--- hydra_core_ui/src/CryptoList.js
+++ hydra_core_ui/src/CryptoList.js
@@ -1,6 +1,27 @@
 import React from 'react';
-
 import axios from 'axios';
+import {GridList, GridTile} from 'material-ui/GridList';
+import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
+import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+const styles = {
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+  },
+  gridList: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    overflowX: 'auto',
+  },
+  titleStyle: {
+    color: 'rgb(0, 188, 212)',
+  },
+};
+
 
 export default class CryptoList extends React.Component {
   state = {
@@ -18,11 +39,16 @@ export default class CryptoList extends React.Component {
 
   render() {
     return (
-    <div>
-        <div className="CryptoListComponent">
-            {this.state.cryptosList.map(crypto => <div className="crypto" >{crypto.symbol1} Currency: {crypto.symbol2}</div>)}
-        </div>
-    </div>
+      <div className="AllCryptos">
+        <MuiThemeProvider className="Root">
+            <GridList className="CryptoList" cols={2.2}>
+              <Subheader>Choose your crypto currency to initiate arbitrage</Subheader>
+              {this.state.cryptosList.map(crypto => (
+                <GridTile className="Crypto" title={crypto.symbol1} subtitle={<span><b>{crypto.symbol2}</b></span>}/>
+              ))}
+            </GridList>
+        </MuiThemeProvider>
+      </div>
     )
   }
 }
